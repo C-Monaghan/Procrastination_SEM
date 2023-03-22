0aa622919e822e0e30f33b46cea6470f9fd2f013
C-Monaghan <cormacmonaghan@tutanota.com>
2023-03-22 10:47:24 +0000

    Removing intercepts

--- 02__Models/03__Growth_Model/1. PP_Growth_Model.R
+++ 02__Models/03__Growth_Model/1. PP_Growth_Model.R
@@ -24,12 +24,15 @@ hrs_data_reduced_2 <- hrs_data[rowSums(is.na(hrs_data[11:15])) < 5 | rowSums(is.
 # Note: LS = Life Satisfaction // Con = Conscientiousness // Neu = Neuroticism // Procra = Procrastination
 pp_model <- '
   # Latent Variables:
-  LS_w1 =~ v1*LS1_w1 + v2*LS2_w1 + v3*LS3_w1 + v4*LS4_w1 + v5*LS5_w1;
-  LS_w2 =~ v1*LS1_w2 + v2*LS2_w2 + v3*LS3_w2 + v4*LS4_w2 + v5*LS5_w2;
+  LS_w1 =~ v1*LS1_w1 + v2*LS2_w1 + v3*LS3_w1 + LS4_w1 + LS5_w1;
+  LS_w2 =~ v1*LS1_w2 + v2*LS2_w2 + v3*LS3_w2 + LS4_w2 + LS5_w2;
+
   Con_w1 =~ v6*Reckless_w1 + v7*Organised_w1 + v8*Responsible_w1 + v9*Hardworking_w1 + v10*Self_disiplined_w1 + v11*Careless_w1 + v12*Impulsive_w1 + v13*Cautious_w1 + v14*Thorough_w1 + v15*Thrifty_w1;
   Con_w2 =~ v6*Reckless_w2 + v7*Organised_w2 + v8*Responsible_w2 + v9*Hardworking_w2 + v10*Self_disiplined_w2 + v11*Careless_w2 + v12*Impulsive_w2 + v13*Cautious_w2 + v14*Thorough_w2 + v15*Thrifty_w2;
+  
   Neu_w1 =~ v16*Moody_w1 + v17*Worrying_w1 + v18*Nervous_w1 + v19*Calm_w1;
   Neu_w2 =~ v16*Moody_w2 + v17*Worrying_w2 + v18*Nervous_w2 + v19*Calm_w2;
+  
   Procra =~ Procras_1 + Procras_2 + Procras_3 + Procras_4 + Procras_5 + Procras_6 + Procras_7 + Procras_8 + Procras_9 + Procras_10 + Procras_11 + Procras_12;
   
   # Residuals at different time points allowed to correlate: 
@@ -38,11 +41,11 @@ pp_model <- '
   LS3_w1 ~~ LS3_w2;
   LS4_w1 ~~ LS4_w2;
   LS5_w1 ~~ LS5_w2;
-  
+
   Reckless_w1 ~~ Reckless_w2;
   Organised_w1 ~~ Organised_w2;
   Responsible_w1 ~~ Responsible_w2;
-  Hardworking_w1 ~~ Hardworking_w2
+  Hardworking_w1 ~~ Hardworking_w2;
   Self_disiplined_w1 ~~ Self_disiplined_w2;
   Careless_w1 ~~ Careless_w2;
   Impulsive_w1 ~~ Impulsive_w2;
@@ -56,53 +59,58 @@ pp_model <- '
   Calm_w1 ~~ Calm_w2;
   
   # Intercepts & Slopes
-  inter_ls =~ 1*LS_w1 + 1*LS_w2;
-  slope_ls =~ 0*LS_w1 + 1*LS_w2;
+  # inter_ls =~ 1*LS_w1 + 1*LS_w2;
+  slope_ls =~ 1*LS_w1 + 2*LS_w2;
   
-  inter_con =~ 1*Con_w1 + 1*Con_w2;
-  slope_con =~ 0*Con_w1 + 1*Con_w2;
+  # inter_con =~ 1*Con_w1 + 1*Con_w2;
+  slope_con =~ 1*Con_w1 + 2*Con_w2;
   
-  inter_neu =~ 1*Neu_w1 + 1*Neu_w2;
-  slope_neu =~ 0*Neu_w1 + 1*Neu_w2;
+  # inter_neu =~ 1*Neu_w1 + 1*Neu_w2;
+  slope_neu =~ 1*Neu_w1 + 2*Neu_w2;
   
   # Direct Effect
-  Procra ~ c*Age_w1 + b1*slope_con + b2*slope_neu + b3*slope_ls;
+  Procra ~ c*Age_w1 + b1*slope_ls + b2*slope_con + b3*slope_neu;
   
   # A Paths
-  slope_con ~ a1*Age_w1;
-  slope_neu ~ a2*Age_w1;
-  slope_ls ~ a3*Age_w1;
+  slope_ls ~ a1*Age_w1;
+  slope_con ~ a2*Age_w1;
+  slope_neu ~ a3*Age_w1;
   
   # Indirect effect
   
-  Indirect_Effect := (a1*b1) + (a2*b2) + (a3*b3) 
+  Indirect_Effect := (a1*b1) + (a2*b2) + (a3*b3)
   
   # Total Effect
   
-  Total_Effect := c + (a1*b1) + (a2*b2) + (a3*b3) 
+  Total_Effect := c + (a1*b1)+ (a2*b2) + (a3*b3)
   
   '
 
-# Latent Growth Model
-pp_model_fit_1 <- growth(pp_model, data = hrs_data_no_missing, estimator = 'ML', missing = "fiml")
-pp_model_fit_2 <- growth(pp_model, data = hrs_data_reduced_1, estimator = 'ML', missing = "fiml")
+# Latent Growth Models
+# pp_model_fit_1 <- growth(pp_model, data = hrs_data_no_missing, estimator = 'ML', missing = "fiml")
+# summary(pp_model_fit_1, fit.measures = TRUE, standardized = TRUE, modindices = FALSE, rsquare = TRUE)
+
+# pp_model_fit_2 <- growth(pp_model, data = hrs_data_reduced_1, estimator = 'ML', missing = "fiml")
+# summary(pp_model_fit_2, fit.measures = TRUE, standardized = TRUE, modindices = FALSE, rsquare = TRUE)
+
 pp_model_fit_3 <- growth(pp_model, data = hrs_data_reduced_2, estimator = 'ML', missing = "fiml")
+model_summary <- summary(pp_model_fit_3, fit.measures = TRUE, standardized = TRUE, modindices = FALSE, rsquare = TRUE)
 
-
-summary(pp_model_fit_2, fit.measures = TRUE, standardized = TRUE, modindices = FALSE, rsquare = TRUE)
+# pp_model_fit_4 <- growth(pp_model, data = hrs_data, estimator = 'ML', missing = "fiml")
+# 
+# pp_model_fit_5 <- growth(pp_model, data = hrs_complete, estimator = 'ML', missing = "fiml")
+# summary(pp_model_fit_5, fit.measures = TRUE, standardized = TRUE, modindices = FALSE, rsquare = TRUE)
 
 # Making a SEM Plot (Check if better way to do this)
-sem_plot <- lavaanPlot(model = pp_model_fit_2,
+sem_plot <- lavaanPlot(model = pp_model_fit_3,
                        node_options = list(shape = "box", fontname = "Helvetica"),
                        edge_options = list(color = "grey"),
                        coefs = TRUE)
-
-# Exporting -------------------------------------------------------------------
+# 
+# # Exporting -------------------------------------------------------------------
 export_path <- "./02__Models/03__Growth_Model/"
 
 save_png(sem_plot, file.path(export_path, "Results/SEM_plot.png"))
-
-
 
 # Notes -----------------------------------------------------------------------
 
@@ -121,3 +129,14 @@ save_png(sem_plot, file.path(export_path, "Results/SEM_plot.png"))
 # 3. Answered the questionnaire in 2016 but not 2012
 
 # The overall hope was to use MICE to impute this missingness
+
+# TESTING ----------------------------------------------------------------------
+
+# cov_mat <- lavInspect(pp_model_fit_3, "vcov")
+# 
+# e_values <- eigen(cov_mat)$values
+# e_values <- Re(e_values)
+# e_values_neg <- e_values[e_values < 0]
+# 
+# print(e_values)
+# print(e_values_neg)
