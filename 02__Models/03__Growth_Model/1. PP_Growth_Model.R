98b220b6534d3156ed752329bf76fb19c01ad871
C-Monaghan <cormacmonaghan@tutanota.com>
2023-03-23 16:27:36 +0000

    Updating model

--- 02__Models/03__Growth_Model/1. PP_Growth_Model.R
+++ 02__Models/03__Growth_Model/1. PP_Growth_Model.R
@@ -24,52 +24,52 @@ hrs_data_reduced_2 <- hrs_data[rowSums(is.na(hrs_data[11:15])) < 5 | rowSums(is.
 # Note: LS = Life Satisfaction // Con = Conscientiousness // Neu = Neuroticism // Procra = Procrastination
 pp_model <- '
   # Latent Variables:
-  LS_w1 =~ v1*LS1_w1 + v2*LS2_w1 + v3*LS3_w1 + LS4_w1 + LS5_w1;
-  LS_w2 =~ v1*LS1_w2 + v2*LS2_w2 + v3*LS3_w2 + LS4_w2 + LS5_w2;
+  # LS_w1 =~ v1*LS1_w1 + v2*LS2_w1 + v3*LS3_w1 + v4*LS4_w1 + v5*LS5_w1;
+  # LS_w2 =~ v1*LS1_w2 + v2*LS2_w2 + v3*LS3_w2 + v4*LS4_w2 + v5*LS5_w2;
 
-  Con_w1 =~ v6*Reckless_w1 + v7*Organised_w1 + v8*Responsible_w1 + v9*Hardworking_w1 + v10*Self_disiplined_w1 + v11*Careless_w1 + v12*Impulsive_w1 + v13*Cautious_w1 + v14*Thorough_w1 + v15*Thrifty_w1;
+  # Con_w1 =~ v6*Reckless_w1 + v7*Organised_w1 + v8*Responsible_w1 + v9*Hardworking_w1 + v10*Self_disiplined_w1 + v11*Careless_w1 + v12*Impulsive_w1 + v13*Cautious_w1 + v14*Thorough_w1 + v15*Thrifty_w1;
   Con_w2 =~ v6*Reckless_w2 + v7*Organised_w2 + v8*Responsible_w2 + v9*Hardworking_w2 + v10*Self_disiplined_w2 + v11*Careless_w2 + v12*Impulsive_w2 + v13*Cautious_w2 + v14*Thorough_w2 + v15*Thrifty_w2;
   
-  Neu_w1 =~ v16*Moody_w1 + v17*Worrying_w1 + v18*Nervous_w1 + v19*Calm_w1;
+  # Neu_w1 =~ v16*Moody_w1 + v17*Worrying_w1 + v18*Nervous_w1 + v19*Calm_w1;
   Neu_w2 =~ v16*Moody_w2 + v17*Worrying_w2 + v18*Nervous_w2 + v19*Calm_w2;
   
   Procra =~ Procras_1 + Procras_2 + Procras_3 + Procras_4 + Procras_5 + Procras_6 + Procras_7 + Procras_8 + Procras_9 + Procras_10 + Procras_11 + Procras_12;
   
   # Residuals at different time points allowed to correlate: 
-  LS1_w1 ~~ LS1_w2;
-  LS2_w1 ~~ LS2_w2;
-  LS3_w1 ~~ LS3_w2;
-  LS4_w1 ~~ LS4_w2;
-  LS5_w1 ~~ LS5_w2;
-
-  Reckless_w1 ~~ Reckless_w2;
-  Organised_w1 ~~ Organised_w2;
-  Responsible_w1 ~~ Responsible_w2;
-  Hardworking_w1 ~~ Hardworking_w2;
-  Self_disiplined_w1 ~~ Self_disiplined_w2;
-  Careless_w1 ~~ Careless_w2;
-  Impulsive_w1 ~~ Impulsive_w2;
-  Cautious_w1 ~~ Cautious_w2;
-  Thorough_w1 ~~ Thorough_w2;
-  Thrifty_w1 ~~ Thrifty_w2;
-  
-  Moody_w1 ~~ Moody_w2;
-  Worrying_w1 ~~ Worrying_w2;
-  Nervous_w1 ~~ Nervous_w2;
-  Calm_w1 ~~ Calm_w2;
+  # LS1_w1 ~~ LS1_w2;
+  # LS2_w1 ~~ LS2_w2;
+  # LS3_w1 ~~ LS3_w2;
+  # LS4_w1 ~~ LS4_w2;
+  # LS5_w1 ~~ LS5_w2;
+  # 
+  # Reckless_w1 ~~ Reckless_w2;
+  # Organised_w1 ~~ Organised_w2;
+  # Responsible_w1 ~~ Responsible_w2;
+  # Hardworking_w1 ~~ Hardworking_w2;
+  # Self_disiplined_w1 ~~ Self_disiplined_w2;
+  # Careless_w1 ~~ Careless_w2;
+  # Impulsive_w1 ~~ Impulsive_w2;
+  # Cautious_w1 ~~ Cautious_w2;
+  # Thorough_w1 ~~ Thorough_w2;
+  # Thrifty_w1 ~~ Thrifty_w2;
+  # 
+  # Moody_w1 ~~ Moody_w2;
+  # Worrying_w1 ~~ Worrying_w2;
+  # Nervous_w1 ~~ Nervous_w2;
+  # Calm_w1 ~~ Calm_w2;
   
   # Intercepts & Slopes
   # inter_ls =~ 1*LS_w1 + 1*LS_w2;
-  slope_ls =~ 1*LS_w1 + 2*LS_w2;
+  # slope_ls =~ 1*LS_w1 + 2*LS_w2;
   
   # inter_con =~ 1*Con_w1 + 1*Con_w2;
-  slope_con =~ 1*Con_w1 + 2*Con_w2;
+  # slope_con =~ 1*Con_w1 + 2*Con_w2;
   
   # inter_neu =~ 1*Neu_w1 + 1*Neu_w2;
-  slope_neu =~ 1*Neu_w1 + 2*Neu_w2;
+  # slope_neu =~ 1*Neu_w1 + 2*Neu_w2;
   
   # Direct Effect
-  Procra ~ c*Age_w1 + b1*slope_ls + b2*slope_con + b3*slope_neu;
+  Procra ~ c*Age_w1 + b1*con_w2 + b2*neu_w2; # + b3*slope_neu;
   
   # A Paths
   slope_ls ~ a1*Age_w1;
