acfe503db3d65032c40ad2bcf3272cf94f00ddf2
C-Monaghan <cormacmonaghan@tutanota.com>
2023-04-17 22:41:05 +0100

    Generating Descriptives

--- 02__Models/01__Processing/03__Experimenting/01__w2_w3_analysis/04__Descriptives.R
+++ 02__Models/01__Processing/03__Experimenting/01__w2_w3_analysis/04__Descriptives.R
@@ -2,42 +2,46 @@
 # -----------------------------------------------------------------------------
 rm(list=ls()) # Clearing work space
 
-library(apaTables)
+library(dplyr)
+library(gtsummary)
 
 path_data <- "./01__Data/03__Experimenting/"
 
 # Reading in data -------------------------------------------------------------
 hrs_data <- readxl::read_xlsx(file.path(path_data, "HRS_Data_Longitudinal.xlsx"))
 
-# Creating missing value counter (for later use) ------------------------------
-count_missing <- function(row) {
-  sum(is.na(row))
-}
-
 # Creating initial total columns ----------------------------------------------
-hrs_data$Depression_total <- rowSums(hrs_data[, 11:18], na.rm = TRUE)
-hrs_data$Loneliness_total <- rowSums(hrs_data[, 19:21], na.rm = TRUE)
-hrs_data$Procrastination_total <- rowSums(hrs_data[, 22:33], na.rm = TRUE)
+hrs_data <- hrs_data %>%
+  mutate(Depression_total = rowSums(select(., starts_with("Depression")), na.rm = TRUE),
+         Procrastination_total = rowSums(select(., starts_with("Procras")), na.rm = TRUE))
 
 # Any total columns equal to 0 are assigned as missing (NA)
-hrs_data <- hrs_data |>
-  dplyr::mutate(across(c("Depression_total","Loneliness_total", "Procrastination_total"), 
-                       ~ ifelse(. %in% 0, NA, .)))
+hrs_data <- hrs_data %>%
+  mutate(across(c("Depression_total",
+                  "Procrastination_total"), ~ if_else(. %in% 0, NA, .)))
 
-# Correcting total column for loneliness --------------------------------------
-# According to the HRS Leave Behind Questionnaire handbook, to properly scale the 
-# values from the 3-Item UCLA Loneliness Scale any final score with 1 or more missing items 
-# should be assigned as missing itself.
+# Generating descriptive
+# Mean, Median, Range
+summary(select(hrs_data, Age_w2, Health_assessment, Procrastination_total, Depression_total))
 
-missing_count <- apply(hrs_data[, 19:21], 1, count_missing)
+# Standard Deviations
+sd <- summarise(select(hrs_data, Age_w2, Health_assessment, Procrastination_total, Depression_total),
+                across(everything(), sd, na.rm = TRUE))
 
-hrs_data$Loneliness_total[missing_count >= 1] <- NA
+# Standard error of mean
+hrs_data %>%
+  select(Age_w2, Health_assessment, Procrastination_total, Depression_total) %>%
+  apply(2, function(x) sd(x, na.rm = TRUE)/sqrt(length(x)))
 
-# Creating a descriptive statistics table
-hrs_data_table <- hrs_data |>
-  dplyr::select(Gender, Education, Marital_status, Age_w2, Life_satisfaction, 
-                Health_assessment, Job_status, Depression_total, 
-                Loneliness_total, Procrastination_total)
+# Frequency table for categorical variables
+hrs_data %>%
+  select(Education, Marital_status, Job_status) %>%
+  table()
 
-apa
 
+d_table <- tbl_summary(
+  hrs_data %>%
+    select(Age_w2, Procrastination_total) %>%
+    summarise(mean = mean(c(Age_w2, Procrastination_total))))
+
+print()
