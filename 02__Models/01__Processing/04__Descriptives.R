0fe8fb4b954720890b0d83079e56225e3af19de5
C-Monaghan <cormacmonaghan@tutanota.com>
2023-05-31 14:27:50 +0100

    Testing

--- 02__Models/01__Processing/04__Descriptives.R
+++ 02__Models/01__Processing/04__Descriptives.R
@@ -10,10 +10,23 @@ path_data <- "./01__Data/02__Processed_data/"
 # Reading in data -------------------------------------------------------------
 hrs_data <- readxl::read_xlsx(file.path(path_data, "HRS_Data_Longitudinal.xlsx"))
 
-# Data manipulation -----------------------------------------------------------
-# Initially, we want to remove the participants with missing values in their covariates
-# Then we create summation columns for our dependent variables, setting those columns with
-# a value of 0 to NA, and then selecting only the necessary data columns
+# Testing ---------------------------------------------------------------------
+test_data <- hrs_data %>%
+  filter(complete.cases(Marital_status, Job_status)) %>%
+  mutate(Procrastination_total = rowSums(select(., starts_with("Procras")), na.rm = TRUE)) %>%
+  mutate(across("Procrastination_total", ~ if_else(. %in% 0, NA, .))) %>%
+  select(Age_w2, Education, Marital_status, Job_status, Procrastination_total) %>%
+  mutate(Education = as.factor(Education), Marital_status = as.factor(Marital_status), 
+         Job_status = as.factor(Job_status))
+
+ggplot(data = test_data, aes(x = Education, y = Procrastination_total, fill = Marital_status)) +
+  geom_bar(stat = "summary", fun = "mean", position = "dodge") +
+  facet_wrap(~ Job_status) +
+  labs(x = "College Education", y = "Mean Procrastination Score", fill = "Marital Status") +
+  scale_fill_manual(values = c("darkblue", "lightblue")) +
+  theme_minimal()
+
+# Getting descriptives
 hrs_data_descript <- hrs_data %>%
   filter(complete.cases(Marital_status, Job_status)) %>%
   mutate(Depression_total = rowSums(select(., starts_with("Depression")), na.rm = TRUE),
@@ -21,28 +34,32 @@ hrs_data_descript <- hrs_data %>%
          Procrastination_total = rowSums(select(., starts_with("Procras")), na.rm = TRUE)) %>%
   mutate(across(c("Depression_total", "Loneliness_total", "Procrastination_total"), 
                 ~ if_else(. %in% 0, NA, .))) %>%
-  select(Gender, Age_w2, Education, Marital_status, Job_status, 
-         Depression_total, Loneliness_total, Procrastination_total)
+  select(Age_w2, Depression_total, Loneliness_total, Procrastination_total)
 
-# Calculate mean scores for numerical variables
 hrs_data_descript %>%
-  summarise(mean_depression = mean(Depression_total, na.rm = TRUE),
+  summarise(mean_age = mean(Age_w2, na.rm = TRUE),
+            mean_depression = mean(Depression_total, na.rm = TRUE),
             mean_loneliness = mean(Loneliness_total, na.rm = TRUE),
             mean_procrastination = mean(Procrastination_total, na.rm = TRUE))
 
-hrs_data_descript %>%
-  summarise(sd_gender = sd(Gender, na.rm = TRUE))
 
-# Testing ---------------------------------------------------------------------
-hrs_data %>%
-  filter(complete.cases(Marital_status, Job_status)) %>%
-  mutate(Procrastination_total = rowSums(select(., starts_with("Procras")), na.rm = TRUE)) %>%
-  mutate(across("Procrastination_total", ~ if_else(. %in% 0, NA, .))) %>%
-  select(Age_w2, Procrastination_total) %>%
-  group_by(Age_w2) %>%
-  summarise(mean_procrastination = mean(Procrastination_total, na.rm = TRUE)) %>%
-  filter(complete.cases(mean_procrastination)) %>%
-  ggplot(aes(x = Age_w2, y = mean_procrastination)) +
-  geom_line() +
-  scale_x_continuous(breaks = seq(25, 95, by = 10))
+t.test(hrs_data_descript$Loneliness_total)$conf.int
 
+
+stderror <- function(x) sd(x)/sqrt(length(x))
+
+stderror(hrs_data_descript$Procrastination_total)
+
+
+c <- hrs_data_descript$Procrastination_total
+
+c <- select(complete.cases(c) == TRUE)
+
+plotrix::std.error(hrs_data_descript$Loneliness_total, na.rm = TRUE)
+
+
+
+median(hrs_data_descript$Loneliness_total, na.rm = TRUE)
+
+
+sd(hrs_data_descript$Loneliness_total, na.rm = TRUE)
