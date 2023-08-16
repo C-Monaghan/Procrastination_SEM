a948096f433573ed2571324a0bbe2d322f06f809
C-Monaghan <cormacmonaghan@tutanota.com>
2023-08-16 09:44:14 +0100

    Updating descriptives

--- 02__Models/01__Processing/04__Descriptives.R
+++ 02__Models/01__Processing/04__Descriptives.R
@@ -1,5 +1,3 @@
-# The below code generated descriptive statistics
-# -----------------------------------------------------------------------------
 rm(list=ls()) # Clearing work space
 
 library(dplyr)
@@ -7,59 +5,44 @@ library(ggplot2)
 
 path_data <- "./01__Data/02__Processed_data/"
 
-# Reading in data -------------------------------------------------------------
+# Reading in data --------------------------------------------------------------
 hrs_data <- readxl::read_xlsx(file.path(path_data, "HRS_Data_Longitudinal.xlsx"))
 
-# Testing ---------------------------------------------------------------------
-test_data <- hrs_data %>%
+# Creating descriptive --------------------------------------------------------
+descriptives <- hrs_data %>%
   filter(complete.cases(Marital_status, Job_status)) %>%
-  mutate(Procrastination_total = rowSums(select(., starts_with("Procras")), na.rm = TRUE)) %>%
-  mutate(across("Procrastination_total", ~ if_else(. %in% 0, NA, .))) %>%
-  select(Age_w2, Education, Marital_status, Job_status, Procrastination_total) %>%
-  mutate(Education = as.factor(Education), Marital_status = as.factor(Marital_status), 
-         Job_status = as.factor(Job_status))
-
-ggplot(data = test_data, aes(x = Education, y = Procrastination_total, fill = Marital_status)) +
-  geom_bar(stat = "summary", fun = "mean", position = "dodge") +
-  facet_wrap(~ Job_status) +
-  labs(x = "College Education", y = "Mean Procrastination Score", fill = "Marital Status") +
-  scale_fill_manual(values = c("darkblue", "lightblue")) +
-  theme_minimal()
-
-# Getting descriptives
-hrs_data_descript <- hrs_data %>%
-  filter(complete.cases(Marital_status, Job_status)) %>%
+  mutate(age_group = cut(Age_w2, breaks = c(28, 40, 50, 60, 70, 80, 92),
+                         labels = c("28-40", "41-50", "51-60", "61-70", "71-80", "80+"),
+                         include.lowest = TRUE)) %>%
   mutate(Depression_total = rowSums(select(., starts_with("Depression")), na.rm = TRUE),
          Loneliness_total = rowSums(select(., starts_with("Loneliness")), na.rm = TRUE),
          Procrastination_total = rowSums(select(., starts_with("Procras")), na.rm = TRUE)) %>%
   mutate(across(c("Depression_total", "Loneliness_total", "Procrastination_total"), 
                 ~ if_else(. %in% 0, NA, .))) %>%
-  select(Age_w2, Depression_total, Loneliness_total, Procrastination_total)
+  select(Age_w2, age_group, Depression_total, Loneliness_total, Procrastination_total)
 
-hrs_data_descript %>%
-  summarise(mean_age = mean(Age_w2, na.rm = TRUE),
-            mean_depression = mean(Depression_total, na.rm = TRUE),
-            mean_loneliness = mean(Loneliness_total, na.rm = TRUE),
-            mean_procrastination = mean(Procrastination_total, na.rm = TRUE))
+# Creating dataset with means --------------------------------------------------
+means <- descriptives %>%
+  group_by(age_group) %>%
+  summarise(Procrastination = mean(Procrastination_total, na.rm = TRUE),
+            Depression = mean(Depression_total, na.rm = TRUE),
+            Loneliness = mean(Loneliness_total, na.rm = TRUE)) %>%
+  tidyr::pivot_longer(cols = -age_group,
+                      names_to = "variable",
+                      values_to = "mean_score")
 
+# Plotting graphs --------------------------------------------------------------
+distribution <- ggplot(descriptives, aes(x = Procrastination_total)) +
+  geom_density(alpha = 0.7, fill = "#8F0A0A") +
+  facet_wrap(~ age_group) +
+  labs(title = "Distribution of Procrastination Scores Across Age",
+       x = "Total Procrastination",
+       y = "Density") +
+  theme_bw() +
+  ggeasy::easy_center_title()
 
-t.test(hrs_data_descript$Loneliness_total)$conf.int
+# Exporting
+export_path <- "./02__Models/03__SEM/"
 
-
-stderror <- function(x) sd(x)/sqrt(length(x))
-
-stderror(hrs_data_descript$Procrastination_total)
-
-
-c <- hrs_data_descript$Procrastination_total
-
-c <- select(complete.cases(c) == TRUE)
-
-plotrix::std.error(hrs_data_descript$Loneliness_total, na.rm = TRUE)
-
-
-
-median(hrs_data_descript$Loneliness_total, na.rm = TRUE)
-
-
-sd(hrs_data_descript$Loneliness_total, na.rm = TRUE)
+cowplot::save_plot(filename = file.path(export_path, "results/03__distribution.png"),
+                   plot = distribution)
